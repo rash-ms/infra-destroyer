d22ae2c9a7d8955ba8b6f9647addbded086167ed
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-05-25 01:06:01 +0100

    change trigger.json

--- routes/trigger.js
+++ routes/trigger.js
@@ -4,7 +4,8 @@ module.exports = async function (req, res) {
   // const { token, infra_dir } = req.body;
   const token = req.body?.token || req.query?.token;
   // const infra_dir = req.body?.infra_dir || req.query?.infra_dir;
-  const infra_dir = req.body.infra_dir;
+  // const infra_dir = req.body.infra_dir;
+  const infra_dir = req.query.infra_dir; // optional if not passed
 
   // Debug the token mismatch
   console.log("Received token:", `"${token}"`);
